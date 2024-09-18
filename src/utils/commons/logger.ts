4a5bd89a021d9136ca8dfa557fa15b813ff865ed
codershani <codershani13@gmail.com>
2024-09-18 21:37:35 +0530

    feat: Added logger configuration for log file

--- src/utils/commons/logger.ts
+++ src/utils/commons/logger.ts
@@ -1,6 +1,9 @@
 import { createLogger, format, transports } from 'winston';
-import { ConsoleTransportInstance } from 'winston/lib/winston/transports';
+import { ConsoleTransportInstance, FileTransportInstance } from 'winston/lib/winston/transports';
 import util from 'util';
+import { ServerConfig } from '../../config';
+import { EApplicationEnvironment } from '../constants';
+import path from 'path';
 
 const consoleLogFormat = format.printf((info) => {
     const { level, message, timestamp, meta = {} } = info;
@@ -18,10 +21,49 @@ const consoleLogFormat = format.printf((info) => {
 });
 
 const consoleTransport = (): Array<ConsoleTransportInstance> => {
+    if (ServerConfig.ENV === EApplicationEnvironment.DEVELOPMENT) {
+        return [
+            new transports.Console({
+                level: 'info',
+                format: format.combine(format.timestamp(), consoleLogFormat),
+            }),
+        ];
+    }
+    return [];
+};
+
+const fileLogFormat = format.printf((info) => {
+    const { level, message, timestamp, meta = {} } = info;
+
+    const logMeta: Record<string, unknown> = {};
+
+    for (const [key, value] of Object.entries(meta)) {
+        if (value instanceof Error) {
+            logMeta[key] = {
+                name: value.name,
+                message: value.message,
+                trace: value.stack,
+            };
+        }
+        logMeta[key] = value;
+    }
+
+    const logData = {
+        level: level.toUpperCase(),
+        message,
+        timestamp,
+        meta: logMeta,
+    };
+
+    return JSON.stringify(logData, null, 4);
+});
+
+const fileTransport = (): Array<FileTransportInstance> => {
     return [
-        new transports.Console({
+        new transports.File({
             level: 'info',
-            format: format.combine(format.timestamp(), consoleLogFormat),
+            filename: path.join(__dirname, '../', '../', '../', 'logs', `${ServerConfig.ENV}.log`),
+            format: format.combine(format.timestamp(), fileLogFormat),
         }),
     ];
 };
@@ -30,5 +72,5 @@ export default createLogger({
     defaultMeta: {
         meta: {},
     },
-    transports: [...consoleTransport()],
+    transports: [...fileTransport(), ...consoleTransport()],
 });
