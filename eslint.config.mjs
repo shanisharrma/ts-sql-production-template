7fba612642b00b9db60bd5d76b7ded76787c10b8
codershani <codershani13@gmail.com>
2024-09-18 16:41:46 +0530

    feat: Configured prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,18 @@
-import eslint from "@eslint/js";
-import tseslint from "typescript-eslint";
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default tseslint.config({
-  languageOptions: {
-    parserOptions: {
-      project: true,
-      tsconfigRootDir: import.meta.dirname,
+    languageOptions: {
+        parserOptions: {
+            project: true,
+            tsconfigRootDir: import.meta.dirname,
+        },
     },
-  },
-  files: ["**/*.ts"],
-  extends: [eslint.configs.recommended, ...tseslint.configs.recommended],
-  rules: {
-    "no-console":
-      "error" /* while pushing to github there shouldn't be any console log statements */,
-    quotes: ["error", "single", { allowTemplateLiterals: true }],
-  },
+    files: ['**/*.ts'],
+    extends: [eslint.configs.recommended, ...tseslint.configs.recommended, eslintConfigPrettier],
+    rules: {
+        'no-console': 'error' /* while pushing to github there shouldn't be any console log statements */,
+        quotes: ['error', 'single', { allowTemplateLiterals: true }],
+    },
 });
