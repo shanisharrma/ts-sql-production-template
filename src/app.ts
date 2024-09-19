c59652eb45ac9f7dfc9bc5beed2102bdd7a420b8
codershani <codershani13@gmail.com>
2024-09-19 20:38:44 +0530

    feat: Added HelmetJS to secure apps

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,12 @@ import express from 'express';
 import path from 'path';
 import apiRoutes from './routes';
 import { GlobalErrorHandler, NotFoundErrorHandler } from './middlewares';
+import helmet from 'helmet';
 
 const app = express();
 
 // Middlewares
+app.use(helmet());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../', 'public')));
 
